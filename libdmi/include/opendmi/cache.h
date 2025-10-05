348076e02e991b25a977065897e6daa8e1eb5e79
Dmitry Sednev <dmitry@sednev.ru>
2025-10-05 20:41:10 +0300

    Updated cache table definitions

--- libdmi/include/opendmi/cache.h
+++ libdmi/include/opendmi/cache.h
@@ -16,21 +16,32 @@
 
 #include <opendmi/table.h>
 
+typedef uint16_t dmi_cache_size_t;
+typedef uint32_t dmi_cache_size_ex_t;
+
+/**
+ * @brief Cache types.
+ */
 enum dmi_cache_type
 {
-    DMI_CACHE_TYPE_OTHER       = 0x01,
-    DMI_CACHE_TYPE_UNKNOWN     = 0x02,
-    DMI_CACHE_TYPE_INSTRUCTION = 0x03,
-    DMI_CACHE_TYPE_DATA        = 0x04,
-    DMI_CACHE_TYPE_UNIFIED     = 0x05
+    DMI_CACHE_TYPE_OTHER       = 0x01, /**< Other */
+    DMI_CACHE_TYPE_UNKNOWN     = 0x02, /**< Unknown */
+    DMI_CACHE_TYPE_INSTRUCTION = 0x03, /**< Instruction cache */
+    DMI_CACHE_TYPE_DATA        = 0x04, /**< Data cache */
+    DMI_CACHE_TYPE_UNIFIED     = 0x05, /**< Unified cache */
+    __DMI_CACHE_TYPE_COUNT
 };
 
+/**
+ * @brief Cache operational modes.
+ */
 enum dmi_cache_mode
 {
-    DMI_CACHE_MODE_WRITE_THROUGH = 0x00,
-    DMI_CACHE_MODE_WRITE_BACK    = 0x01,
-    DMI_CACHE_MODE_VARIABLE      = 0x02,
-    DMI_CACHE_MODE_UNKNOWN       = 0x03
+    DMI_CACHE_MODE_WRITE_THROUGH = 0x00, /**< Write through */
+    DMI_CACHE_MODE_WRITE_BACK    = 0x01, /**< Write back */
+    DMI_CACHE_MODE_VARIABLE      = 0x02, /**< Varies with memory address */
+    DMI_CACHE_MODE_UNKNOWN       = 0x03, /**< Unknown */
+    __DMI_CACHE_MODE_COUNT
 };
 
 /**
@@ -43,25 +54,30 @@ enum dmi_cache_ecc_type
     DMI_CACHE_ECC_NONE       = 0x03, /**< None */
     DMI_CACHE_ECC_PARITY     = 0x04, /**< Parity */
     DMI_CACHE_ECC_SINGLE_BIT = 0x05, /**< Single-bit ECC */
-    DMI_CACHE_ECC_MULTI_BIT  = 0x06  /**< Multi-bit ECC */
+    DMI_CACHE_ECC_MULTI_BIT  = 0x06, /**< Multi-bit ECC */
+    __DMI_CACHE_ECC_TYPE_COUNT
 };
 
+/**
+ * @brief Cache associativity types.
+ */
 enum dmi_cache_assoc
 {
-    DMI_CACHE_ASSOC_OTHER   = 0x01,
-    DMI_CACHE_ASSOC_UNKNOWN = 0x02,
-    DMI_CACHE_ASSOC_DIRECT  = 0x03,
-    DMI_CACHE_ASSOC_2WAY    = 0x04,
-    DMI_CACHE_ASSOC_4WAY    = 0x05,
-    DMI_CACHE_ASSOC_FULL    = 0x06,
-    DMI_CACHE_ASSOC_8WAY    = 0x07,
-    DMI_CACHE_ASSOC_16WAY   = 0x08,
-    DMI_CACHE_ASSOC_12WAY   = 0x09,
-    DMI_CACHE_ASSOC_24WAY   = 0x0A,
-    DMI_CACHE_ASSOC_32WAY   = 0x0B,
-    DMI_CACHE_ASSOC_48WAY   = 0x0C,
-    DMI_CACHE_ASSOC_64WAY   = 0x0D,
-    DMI_CACHE_ASSOC_20WAY   = 0x0E
+    DMI_CACHE_ASSOC_OTHER   = 0x01, /**< Other */
+    DMI_CACHE_ASSOC_UNKNOWN = 0x02, /**< Unknown */
+    DMI_CACHE_ASSOC_DIRECT  = 0x03, /**< Direct mapped */
+    DMI_CACHE_ASSOC_2WAY    = 0x04, /**< 2-way set-associative */
+    DMI_CACHE_ASSOC_4WAY    = 0x05, /**< 4-way set-associative */
+    DMI_CACHE_ASSOC_FULL    = 0x06, /**< Fully associative */
+    DMI_CACHE_ASSOC_8WAY    = 0x07, /**< 8-way set-associative */
+    DMI_CACHE_ASSOC_16WAY   = 0x08, /**< 16-way set-associative */
+    DMI_CACHE_ASSOC_12WAY   = 0x09, /**< 12-way set-associative */
+    DMI_CACHE_ASSOC_24WAY   = 0x0A, /**< 24-way set-associative */
+    DMI_CACHE_ASSOC_32WAY   = 0x0B, /**< 32-way set-associative */
+    DMI_CACHE_ASSOC_48WAY   = 0x0C, /**< 48-way set-associative */
+    DMI_CACHE_ASSOC_64WAY   = 0x0D, /**< 64-way set-associative */
+    DMI_CACHE_ASSOC_20WAY   = 0x0E, /**< 20-way set-associative */
+    __DMI_CACHE_ASSOC_COUNT
 };
 
 /**
@@ -72,9 +88,13 @@ enum dmi_cache_location
     DMI_CACHE_LOCATION_INTERNAL = 0x00, /**< Internal */
     DMI_CACHE_LOCATION_EXTERNAL = 0x01, /**< External */
     DMI_CACHE_LOCATION_RESERVED = 0x02, /**< Reserved */
-    DMI_CACHE_LOCATION_UNKNOWN  = 0x03  /**< Unknown */
+    DMI_CACHE_LOCATION_UNKNOWN  = 0x03, /**< Unknown */
+    __DMI_CACHE_LOCATION_COUNT
 };
 
+/**
+ * @brief Cache configuration.
+ */
 struct dmi_cache_config
 {
     uint8_t level : 3;
@@ -85,10 +105,117 @@ struct dmi_cache_config
     enum dmi_cache_mode mode : 2;
 } __attribute__((packed));
 
+/**
+ * @brief Cache information table (type 7).
+ * 
+ * The information in this structure defines the attributes of CPU cache
+ * device in the system. One structure is specified for each such device,
+ * whether the device is internal to or external to the CPU module. Cache
+ * modules can be associated with a processor structure in one or two ways
+ * depending on the SMBIOS version.
+ */
 struct dmi_cache_table
 {
+    /**
+     * @brief DMI table header.
+     */
     struct dmi_table_header header;
+
+    /**
+     * @brief String number for reference designation, e.g. "CACHE1", 0.
+     * @since SMBIOS 2.0
+     */
+    uint8_t socket;
+
+    /**
+     * @brief Cache configuration.
+     * @since SMBIOS 2.0
+     */
+    struct dmi_cache_config config;
+
+    /**
+     * @brief Maximum cache size that can be installed.
+     * @since SMBIOS 2.0
+     */
+    dmi_cache_size_t maximum_size;
+
+    /**
+     * @brief Installed cache size, set to 0 if no cache is installed.
+     * @since SMBIOS 2.0
+     */
+    dmi_cache_size_t installed_size;
+
+    /**
+     * @brief Supported SRAM type.
+     * @since SMBIOS 2.0
+     */
+    uint16_t supported_sram_type;
+
+    /**
+     * @brief Current SRAM type.
+     * @since SMBIOS 2.0
+     */
+    uint16_t current_sram_type;
+
+    /**
+     * @brief Cache module speed, in nanoseconds. The value is 0 if the speed
+     * is unknown.
+     * @since SMBIOS 2.1
+     */
+    uint8_t speed;
+
+    /**
+     * @brief Error-correction scheme supported by this cache component.
+     * @since SMBIOS 2.1
+     */
+    enum dmi_cache_ecc_type ecc_type;
+
+    /**
+     * @brief Logical type of cache.
+     * @since SMBIOS 2.1
+     */
+    enum dmi_cache_type type;
+
+    /**
+     * @brief Associativity of the cache.
+     * @since SMBIOS 2.1
+     */
+    enum dmi_cache_assoc associativity;
+
+    /**
+     * @brief Maximum cache size that can be installed (for caches larger than
+     * 2047 MiB).
+     * @since SMBIOS 3.1
+     */
+    dmi_cache_size_ex_t maximum_size_ex;
+
+    /**
+     * @brief Installed cache size (for caches larget than 2047 MiB).
+     * @since SMBIOS 3.1
+     */
+    dmi_cache_size_ex_t installed_size_ex;
 } __attribute__((packed));
+
+/**
+ * @brief Decoded cache information.
+ */
+struct dmi_cache_info
+{
+    /**
+     * @brief Socket designator.
+     */
+    char *socket;
+
+    /**
+     * @brief Maximum cache size that can be installed, in bytes.
+     */
+    size_t maximum_size;
+
+    /**
+     * @brief Installed cache size, in bytes.
+     */
+    size_t installed_size;
+};
 
 __BEGIN_DECLS
 
@@ -98,7 +225,11 @@ const char *dmi_cache_ecc_type_name(enum dmi_cache_ecc_type value);
 const char *dmi_cache_assoc_name(enum dmi_cache_assoc value);
 const char *dmi_cache_location_name(enum dmi_cache_location value);
 
-size_t dmi_cache_size(uint16_t value);
+size_t dmi_cache_size(dmi_cache_size_t value);
+size_t dmi_cache_size_ex(dmi_cache_size_ex_t value);
+
+struct dmi_cache_info *dmi_cache_info_create(struct dmi_cache_table *table);
+void dmi_cache_info_destroy(struct dmi_cache_info *info);
 
 __END_DECLS
 
--- libdmi/src/cache.c
+++ libdmi/src/cache.c
@@ -1,7 +1,10 @@
 #include <opendmi/cache.h>
 #include <opendmi/utils.h>
 
-static const char *dmi_cache_types[] =
+#include <stdlib.h>
+#include <errno.h>
+
+static const char *dmi_cache_type_names[__DMI_CACHE_TYPE_COUNT] =
 {
     [DMI_CACHE_TYPE_OTHER]       = "Other",
     [DMI_CACHE_TYPE_UNKNOWN]     = "Unknown",
@@ -10,7 +13,7 @@ static const char *dmi_cache_types[] =
     [DMI_CACHE_TYPE_UNIFIED]     = "Unified"
 };
 
-static const char *dmi_cache_modes[] =
+static const char *dmi_cache_mode_names[__DMI_CACHE_MODE_COUNT] =
 {
     [DMI_CACHE_MODE_WRITE_THROUGH] = "Write-through",
     [DMI_CACHE_MODE_WRITE_BACK]    = "Write-back",
@@ -18,11 +21,110 @@ static const char *dmi_cache_modes[] =
     [DMI_CACHE_MODE_UNKNOWN]       = "Unknown"
 };
 
-size_t dmi_cache_size(uint16_t value)
+static const char *dmi_cache_ecc_type_names[__DMI_CACHE_ECC_TYPE_COUNT] =
 {
-    size_t size = value & 0x7FFF;
+    [DMI_CACHE_ECC_OTHER]      = "Other",
+    [DMI_CACHE_ECC_UNKNOWN]    = "Unknown",
+    [DMI_CACHE_ECC_NONE]       = "None",
+    [DMI_CACHE_ECC_PARITY]     = "Parity",
+    [DMI_CACHE_ECC_SINGLE_BIT] = "Single-bit ECC",
+    [DMI_CACHE_ECC_MULTI_BIT]  = "Multi-bit ECC"
+};
 
-    if (value & 0x8000)
+static const char *dmi_cache_assoc_names[__DMI_CACHE_ASSOC_COUNT] =
+{
+    [DMI_CACHE_ASSOC_OTHER]   = "Other",
+    [DMI_CACHE_ASSOC_UNKNOWN] = "Unknown",
+    [DMI_CACHE_ASSOC_DIRECT]  = "Direct mapped",
+    [DMI_CACHE_ASSOC_2WAY]    = "2-way set-associative",
+    [DMI_CACHE_ASSOC_4WAY]    = "4-way set-associative",
+    [DMI_CACHE_ASSOC_FULL]    = "Fully associative",
+    [DMI_CACHE_ASSOC_8WAY]    = "8-way set-associative",
+    [DMI_CACHE_ASSOC_16WAY]   = "16-way set-associative",
+    [DMI_CACHE_ASSOC_12WAY]   = "12-way set-associative",
+    [DMI_CACHE_ASSOC_24WAY]   = "24-way set-associative",
+    [DMI_CACHE_ASSOC_32WAY]   = "32-way set-associative",
+    [DMI_CACHE_ASSOC_48WAY]   = "48-way set-associative",
+    [DMI_CACHE_ASSOC_64WAY]   = "64-way set-associative",
+    [DMI_CACHE_ASSOC_20WAY]   = "20-way set-associative"
+};
+
+static const char *dmi_cache_location_names[__DMI_CACHE_LOCATION_COUNT] =
+{
+    [DMI_CACHE_LOCATION_INTERNAL] = "Internal",
+    [DMI_CACHE_LOCATION_EXTERNAL] = "External",
+    [DMI_CACHE_LOCATION_RESERVED] = "Reserved",
+    [DMI_CACHE_LOCATION_UNKNOWN]  = "Unknown"
+};
+
+const char *dmi_cache_type_name(enum dmi_cache_type value)
+{
+    return dmi_name(dmi_cache_type_names, value, __DMI_CACHE_TYPE_COUNT);
+}
+
+const char *dmi_cache_mode_name(enum dmi_cache_mode value)
+{
+    return dmi_name(dmi_cache_mode_names, value, __DMI_CACHE_MODE_COUNT);
+}
+
+const char *dmi_cache_ecc_type_name(enum dmi_cache_ecc_type value)
+{
+    return dmi_name(dmi_cache_ecc_type_names, value, __DMI_CACHE_ECC_TYPE_COUNT);
+}
+
+const char *dmi_cache_assoc_name(enum dmi_cache_assoc value)
+{
+    return dmi_name(dmi_cache_assoc_names, value, __DMI_CACHE_ASSOC_COUNT);
+}
+
+const char *dmi_cache_location_name(enum dmi_cache_location value)
+{
+    return dmi_name(dmi_cache_location_names, value, __DMI_CACHE_LOCATION_COUNT);
+}
+
+struct dmi_cache_info *dmi_cache_info_create(struct dmi_cache_table *table)
+{
+    struct dmi_cache_info *info;
+
+    if (!table) {
+        errno = EINVAL;
+        return nullptr;
+    }
+
+    info = malloc(sizeof(struct dmi_cache_info));
+    if (!info) {
+        errno = ENOMEM;
+        return nullptr;
+    }
+
+    return info;
+}
+
+void dmi_cache_info_destroy(struct dmi_cache_info *info)
+{
+    if (!info)
+        return;
+
+    free(info);
+}
+
+size_t dmi_cache_size(dmi_cache_size_t value)
+{
+    size_t size = value & 0x7FFFU;
+
+    if (value & 0x8000U)
+        size <<= 16; // Granularity is 64 Kb
+    else
+        size <<= 10; // Granularity is 1 Kb
+
+    return size;
+}
+
+size_t dmi_cache_size_ex(dmi_cache_size_ex_t value)
+{
+    size_t size = value & 0x7FFFFFFFU;
+
+    if (value & 0x80000000U)
         size <<= 16; // Granularity is 64 Kb
     else
         size <<= 10; // Granularity is 1 Kb
