842c0f06dacba74e3bb4bbb392ea8c94f0a45b43
Dmitry Sednev <dmitry@sednev.ru>
2025-10-26 13:18:10 +0300

    Update file headers

--- libdmi/src/error.c
+++ libdmi/src/error.c
@@ -1,6 +1,6 @@
 #include <opendmi/error.h>
 //
-// OpenDMI: DMI/SMBIOS library
+// OpenDMI: Cross-platform DMI/SMBIOS framework
 // Copyright (c) 2025, Dmitry Sednev <dmitry@sednev.ru>
 //
 // SPDX-License-Identifier: BSD-3-Clause
