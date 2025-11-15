90895aa8ae4d1441f355c8835c5b7560d16359c3
Dmitry Sednev <dmitry@sednev.ru>
2025-11-16 00:33:40 +0300

    Implement system reset table decoding #23

--- libdmi/include/opendmi/table/system-reset.h
+++ libdmi/include/opendmi/table/system-reset.h
@@ -11,15 +11,64 @@
 
 #include <opendmi/table.h>
 
-#ifndef DMI_SYSTEM_RESET_DATA_T
-#define DMI_SYSTEM_RESET_DATA_T
-typedef struct dmi_system_reset_data dmi_system_reset_data_t;
-#endif // !DMI_SYSTEM_RESET_DATA_T
+/**
+ * @brief System boot options.
+ */
+typedef enum dmi_boot_option
+{
+    DMI_BOOT_OPTION_RESERVED         = 0x00, ///< Reserved, do not use
+    DMI_BOOT_OPTION_OPERATING_SYSTEM = 0x01, ///< Operating system
+    DMI_BOOT_OPTION_SYSTEM_UTILS     = 0x02, ///< System utilities
+    DMI_BOOT_OPTION_AVOID_REBOOT     = 0x03, ///< Do not reboot
+} dmi_boot_option_t;
 
-#ifndef DMI_SYSTEM_RESET_T
-#define DMI_SYSTEM_RESET_T
-typedef struct dmi_system_reset dmi_system_reset_t;
-#endif // !DMI_SYSTEM_RESET_T
+/**
+ * @brief System reset capabilities.
+ */
+DMI_PACKED_UNION(dmi_system_reset_caps)
+{
+    /**
+     * @brief Raw value;
+     */
+    dmi_byte_t _value;
+
+    DMI_PACKED_STRUCT()
+    {
+        /**
+         * @brief Status. Identifies whether (`true`) or not (`false`) the
+         * system reset is enabled by the user.
+         */
+        bool status : 1;
+
+        /**
+         * @brief Boot option. Indicates one of the following actions to be
+         * taken after a watchdog reset.
+         */
+        dmi_boot_option_t boot_on_watchdog : 2;
+
+        /**
+         * @brief Boot option on limit. Identifies one of the following system
+         * actions to be taken when the reset limit is reached.
+         */
+        dmi_boot_option_t boot_on_limit : 2;
+
+        /**
+         * @brief System contains a watchdog timer: either `true` or `false`.
+         */
+        bool watchdog_present : 1;
+
+        /**
+         * @brief Reserved for future assignment by this specification, set to
+         * zero.
+         */
+        dmi_byte_t reserved : 2;
+    };
+};
+
+#ifndef DMI_SYSTEM_RESET_CAPS_T
+#define DMI_SYSTEM_RESET_CAPS_T
+typedef union dmi_system_reset_caps dmi_system_reset_caps_t;
+#endif // !DMI_SYSTEM_RESET_CAPS_T
 
 /**
  * @brief System reset table (type 23).
@@ -63,9 +112,80 @@ DMI_PACKED_STRUCT(dmi_system_reset_data)
     dmi_word_t timeout;
 };
 
+#ifndef DMI_SYSTEM_RESET_DATA_T
+#define DMI_SYSTEM_RESET_DATA_T
+typedef struct dmi_system_reset_data dmi_system_reset_data_t;
+#endif // !DMI_SYSTEM_RESET_DATA_T
+
+struct dmi_system_reset
+{
+    /**
+     * @brief Status. Identifies whether (`true`) or not (`false`) the
+     * system reset is enabled by the user.
+     */
+    bool status;
+
+    /**
+     * @brief Boot option. Indicates one of the following actions to be
+     * taken after a watchdog reset.
+     */
+    dmi_boot_option_t boot_on_watchdog;
+
+    /**
+     * @brief Boot option on limit. Identifies one of the following system
+     * actions to be taken when the reset limit is reached.
+     */
+    dmi_boot_option_t boot_on_limit;
+
+    /**
+     * @brief System contains a watchdog timer: either `true` or `false`.
+     */
+    bool watchdog_present;
+
+    /**
+     * @brief Number of automatic system resets since the last intentional
+     * reset. A value of `USHRT_MAX` indicates unknown.
+     */
+    unsigned short reset_count;
+
+    /**
+     * @brief Number of consecutive times the system reset is attempted. A
+     * value of `USHRT_MAX` indicates unknown.
+     */
+    unsigned short reset_limit;
+
+    /**
+     * @brief Number of minutes to use for the watchdog timer. If the timer is
+     * not reset within this interval, the system reset timeout begins. A value
+     * of `USHRT_MAX` indicates unknown.
+     */
+    unsigned short timer_interval;
+
+    /**
+     * @brief Number of minutes before the reboot is initiated. It is used
+     * after a system power cycle, system reset (local or remote), and
+     * automatic system reset. A value of `USHRT_MAX` indicates unknown.
+     */
+    unsigned short timeout;
+};
+
+#ifndef DMI_SYSTEM_RESET_T
+#define DMI_SYSTEM_RESET_T
+typedef struct dmi_system_reset dmi_system_reset_t;
+#endif // !DMI_SYSTEM_RESET_T
+
 /**
  * @brief System reset table specification.
  */
 extern const dmi_table_spec_t dmi_system_reset_table;
+
+__BEGIN_DECLS
+
+const char *dmi_boot_option_name(dmi_boot_option_t value);
+
+dmi_system_reset_t *dmi_system_reset_decode(const dmi_table_t *table);
+void dmi_system_reset_destroy(dmi_system_reset_t *info);
+
+__END_DECLS
 
 #endif // !OPENDMI_TABLE_SYSTEM_RESET_H
--- libdmi/src/table/system-reset.c
+++ libdmi/src/table/system-reset.c
@@ -4,10 +4,80 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 //
+#include <stdlib.h>
+#include <limits.h>
+
+#include <opendmi/name.h>
+#include <opendmi/utils.h>
 #include <opendmi/table/system-reset.h>
 
-const dmi_attribute_t dmi_system_reset_attrs[] =
+static const dmi_name_t dmi_boot_option_names[] =
 {
+    {
+        .id   = DMI_BOOT_OPTION_RESERVED,
+        .code = "reserved",
+        .name = "Reserved"
+    },
+    {
+        .id   = DMI_BOOT_OPTION_OPERATING_SYSTEM,
+        .code = "operating-system",
+        .name = "Operating system"
+    },
+    {
+        .id   = DMI_BOOT_OPTION_SYSTEM_UTILS,
+        .code = "system-utils",
+        .name = "System utilities"
+    },
+    {
+        .id   = DMI_BOOT_OPTION_AVOID_REBOOT,
+        .code = "avoid-reboot",
+        .name = "Do not reboot"
+    },
+    DMI_NAME_NULL
+};
+
+static const dmi_attribute_t dmi_system_reset_attrs[] =
+{
+    DMI_ATTRIBUTE(dmi_system_reset_t, status, BOOL, {
+        .code    = "status",
+        .name    = "Reset enabled"
+    }),
+    DMI_ATTRIBUTE(dmi_system_reset_t, boot_on_watchdog, ENUM, {
+        .code    = "boot-on-watchdog",
+        .name    = "Boot option on watchdog",
+        .values  = dmi_boot_option_names
+    }),
+    DMI_ATTRIBUTE(dmi_system_reset_t, boot_on_limit, ENUM, {
+        .code    = "boot-on-limit",
+        .name    = "Boot option on reset limit",
+        .values  = dmi_boot_option_names
+    }),
+    DMI_ATTRIBUTE(dmi_system_reset_t, watchdog_present, BOOL, {
+        .code    = "watchdog-present",
+        .name    = "Watchdog present"
+    }),
+    DMI_ATTRIBUTE(dmi_system_reset_t, reset_count, INTEGER, {
+        .code    = "reset-count",
+        .name    = "Reset count",
+        .unknown = &(unsigned short){ USHRT_MAX }
+    }),
+    DMI_ATTRIBUTE(dmi_system_reset_t, reset_limit, INTEGER, {
+        .code    = "reset-limit",
+        .name    = "Reset limit",
+        .unknown = &(unsigned short){ USHRT_MAX }
+    }),
+    DMI_ATTRIBUTE(dmi_system_reset_t, timer_interval, INTEGER, {
+        .code    = "timer-interval",
+        .name    = "Timer interval",
+        .unit    = "min",
+        .unknown = &(unsigned short){ USHRT_MAX }
+    }),
+    DMI_ATTRIBUTE(dmi_system_reset_t, timeout, INTEGER, {
+        .code    = "timeout",
+        .name    = "Timeout",
+        .unit    = "min",
+        .unknown = &(unsigned short){ USHRT_MAX },
+    }),
     DMI_ATTRIBUTE_NULL
 };
 
@@ -17,5 +87,44 @@ const dmi_table_spec_t dmi_system_reset_table =
     .name       = "System reset",
     .type       = DMI_TYPE_SYSTEM_RESET,
     .min_length = 0x0D,
-    .attributes = dmi_system_reset_attrs
+    .attributes = dmi_system_reset_attrs,
+    .handlers   = {
+        .decoder     = (dmi_table_decoder_t)dmi_system_reset_decode,
+        .deallocator = (dmi_table_deallocator_t)dmi_system_reset_destroy
+    }
 };
+
+const char *dmi_boot_option_name(dmi_boot_option_t value)
+{
+    return dmi_name_lookup(dmi_boot_option_names, value);
+}
+
+dmi_system_reset_t *dmi_system_reset_decode(const dmi_table_t *table)
+{
+    dmi_system_reset_t *info = nullptr;
+    dmi_system_reset_data_t *data = dmi_cast(data, table->data);
+
+    info = calloc(1, sizeof(*info));
+    if (!info)
+        return nullptr;
+
+    dmi_system_reset_caps_t caps = {
+        ._value = data->capabilities
+    };
+
+    info->status           = caps.status;
+    info->boot_on_watchdog = caps.boot_on_watchdog;
+    info->boot_on_limit    = caps.boot_on_limit;
+    info->watchdog_present = caps.watchdog_present;
+    info->reset_count      = dmi_decode_word(data->reset_count);
+    info->reset_limit      = dmi_decode_word(data->reset_limit);
+    info->timer_interval   = dmi_decode_word(data->timer_inverval);
+    info->timeout          = dmi_decode_word(data->timeout);
+
+    return info;
+}
+
+void dmi_system_reset_destroy(dmi_system_reset_t *info)
+{
+    free(info);
+}
