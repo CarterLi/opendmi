87993bf464b8ba0b5761b0b37c8eea0922616a42
Dmitry Sednev <dmitry@sednev.ru>
2025-11-20 02:56:40 +0300

    Implement memory module structure decoding #29

--- libdmi/include/opendmi/table/memory-module.h
+++ libdmi/include/opendmi/table/memory-module.h
@@ -11,16 +11,79 @@
 
 #include <opendmi/table.h>
 
-#ifndef DMI_MEMORY_MODULE_DATA_T
-#define DMI_MEMORY_MODULE_DATA_T
-typedef struct dmi_memory_module_data dmi_memory_module_data_t;
-#endif // !DMI_MEMORY_MODULE_DATA_T
+/**
+ * @brief Memory module type details.
+ */
+DMI_PACKED_UNION(dmi_memory_module_type)
+{
+    dmi_word_t _value;
 
-#ifndef DMI_MEMORY_MODULE_T
-#define DMI_MEMORY_MODULE_T
-typedef struct dmi_memory_module dmi_memory_module_t;
-#endif // !DMI_MEMORY_MODULE_T
+    DMI_PACKED_STRUCT()
+    {
+        bool is_other      : 1; ///< Other
+        bool is_unknown    : 1; ///< Unknown
+        bool is_standard   : 1; ///< Standard
+        bool is_fast_page  : 1; ///< Fast page mode
+        bool is_edo        : 1; ///< EDO
+        bool has_parity    : 1; ///< Parity
+        bool has_ecc       : 1; ///< ECC
+        bool is_simm       : 1; ///< SIMM
+        bool is_dimm       : 1; ///< DIMM
+        bool is_burst_edo  : 1; ///< Burst EDO
+        bool is_sdram      : 1; ///< SDRAM
 
+        dmi_word_t reserved : 5;
+    };
+};
+
+#ifndef DMI_MEMORY_MODULE_TYPE_T
+#define DMI_MEMORY_MODULE_TYPE_T
+typedef union dmi_memory_module_type dmi_memory_module_type_t;
+#endif // !DMI_MEMORY_MODULE_TYPE_T
+
+/**
+ * @brief Memory module error status.
+ */
+DMI_PACKED_UNION(dmi_memory_module_error)
+{
+    dmi_byte_t _value;
+
+    DMI_PACKED_STRUCT()
+    {
+        /**
+         * @brief Uncorrectable errors received for the module, if set. All or
+         * a portion of the module has been disabled. This bit is only reset on
+         * power-on.
+         */
+        bool has_uncorrectable : 1;
+
+        /**
+         * @brief Correctable errors received for the module, if set. This bit
+         * is reset only during a system reset.
+         */
+        bool has_correctable : 1;
+
+        /**
+         * @brief If set, the error status information should be obtained from
+         * the event log; bits 1and 0 are reserved.
+         */
+        bool has_event_log : 1;
+
+        /**
+         * @brief Reserved, set to zero.
+         */
+        dmi_byte_t reserved : 5;
+    };
+};
+
+#ifndef DMI_MEMORY_MODULE_ERROR_T
+#define DMI_MEMORY_MODULE_ERROR_T
+typedef union dmi_memory_module_error dmi_memory_module_error_t;
+#endif // !DMI_MEMORY_MODULE_ERROR_T
+
+/**
+ * @brief Memory module information table (type 6, obsolete).
+ */
 struct dmi_memory_module_data
 {
     /**
@@ -29,7 +92,7 @@ struct dmi_memory_module_data
     dmi_header_t header;
 
     /**
-     * @brief String number for reference designation. Example: "J202".
+     * @brief String number for socket reference designation. Example: "J202".
      */
     dmi_string_t socket;
 
@@ -49,18 +112,116 @@ struct dmi_memory_module_data
      */
     dmi_byte_t current_speed;
 
+    /**
+     * @brief The physical characteristics of the memory modules that are
+     * supported by (and currently installed in) the system.
+     */
     dmi_word_t current_type;
 
+    /**
+     * @brief The size of the memory module that is installed in the socket.
+     */
     dmi_byte_t installed_size;
 
+    /**
+     * @brief The amount of memory currently enabled for the system’s use from
+     * the module.
+     */
     dmi_byte_t enabled_size;
 
+    /**
+     * @brief Error status.
+     */
     dmi_byte_t error_status;
 };
+
+#ifndef DMI_MEMORY_MODULE_DATA_T
+#define DMI_MEMORY_MODULE_DATA_T
+typedef struct dmi_memory_module_data dmi_memory_module_data_t;
+#endif // !DMI_MEMORY_MODULE_DATA_T
+
+struct dmi_memory_module
+{
+    /**
+     * @brief Socket reference designator. Example: "202".
+     */
+    const char *socket;
+
+    /**
+     * @brief Each nibble indicates a bank (RAS#) connection. 0x0F means no
+     * connection.
+     * 
+     * Example: If banks 1 & 3 (RAS# 1 & 3) were connected to a SIMM socket
+     * the values for that socket would be { 1, 3 }. If only bank 2 (RAS# 2)
+     * were connected, the values for that socket would be { USHRT_MAX, 2 }.
+     */
+    unsigned short bank_connections[2];
+
+    /**
+     * @brief Speed of the memory module, in ns (for example, 70 for a 70ns
+     * module). If the speed is unknown, the field is set to 0.
+     */
+    unsigned short current_speed;
+
+    /**
+     * @brief The physical characteristics of the memory modules that are
+     * supported by (and currently installed in) the system.
+     */
+    dmi_memory_module_type_t current_type;
+
+    /**
+     * @brief The size of the memory module that is installed in the socket.
+     */
+    dmi_size_t installed_size;
+
+    /**
+     * @brief Set to `true` if the installed size is determinable.
+     */
+    bool has_installed_size;
+
+    /**
+     * @brief The amount of memory currently enabled for the system’s use from
+     * the module.
+     */
+    dmi_size_t enabled_size;
+
+    /**
+     * @brief Set to `true` if the enabled size is not determinable.
+     */
+    bool has_enabled_size;
+
+    /**
+     * @brief Set to `true`, if a module is known to be installed in a connector,
+     * but all memory in the module has been disabled due to error.
+     */
+    bool is_disabled;
+
+    /**
+     * @brief Number of bank connections per module.
+     */
+    unsigned short bank_count;
+
+    /**
+     * @brief Error status.
+     */
+    dmi_memory_module_error_t error_status;
+};
+
+#ifndef DMI_MEMORY_MODULE_T
+#define DMI_MEMORY_MODULE_T
+typedef struct dmi_memory_module dmi_memory_module_t;
+#endif // !DMI_MEMORY_MODULE_T
 
 /**
  * @brief Memory module information table specification.
  */
 extern const dmi_table_spec_t dmi_memory_module_table;
+
+__BEGIN_DECLS
+
+dmi_memory_module_t *dmi_memory_module_decode(const dmi_table_t *table);
+void dmi_memory_module_free(dmi_memory_module_t *info);
+
+__END_DECLS
 
 #endif // !OPENDMI_TABLE_MEMORY_MODULE_H
--- libdmi/src/table/memory-module.c
+++ libdmi/src/table/memory-module.c
@@ -4,10 +4,30 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 //
+#include <stdlib.h>
+
+#include <opendmi/utils.h>
 #include <opendmi/table/memory-module.h>
 
 const dmi_attribute_t dmi_memory_module_attrs[] =
 {
+    DMI_ATTRIBUTE(dmi_memory_module_t, socket, STRING, {
+        .code = "socket",
+        .name = "Socket designator"
+    }),
+    DMI_ATTRIBUTE(dmi_memory_module_t, current_speed, INTEGER, {
+        .code = "current-speed",
+        .name = "Current speed",
+        .unit = "ns"
+    }),
+    DMI_ATTRIBUTE(dmi_memory_module_t, installed_size, SIZE, {
+        .code = "installed-size",
+        .name = "Installed size"
+    }),
+    DMI_ATTRIBUTE(dmi_memory_module_t, enabled_size, SIZE, {
+        .code = "enabled-size",
+        .name = "Enabled size"
+    }),
     DMI_ATTRIBUTE_NULL
 };
 
@@ -17,5 +37,65 @@ const dmi_table_spec_t dmi_memory_module_table =
     .name       = "Memory module information",
     .type       = DMI_TYPE_MEMORY_MODULE,
     .min_length = 0x08,
-    .attributes = dmi_memory_module_attrs
+    .attributes = dmi_memory_module_attrs,
+    .handlers = {
+        .decode = (dmi_table_decode_fn_t)dmi_memory_module_decode,
+        .free   = (dmi_table_free_fn_t)dmi_memory_module_free
+    }
 };
+
+dmi_memory_module_t *dmi_memory_module_decode(const dmi_table_t *table)
+{
+    dmi_memory_module_t *info = nullptr;
+    dmi_memory_module_data_t *data = dmi_cast(data, table->data);
+
+    info = calloc(1, sizeof(*info));
+    if (!info)
+        return nullptr;
+
+    info->socket = dmi_table_string(table, data->socket);
+
+    // Decode bank connections
+    dmi_byte_t bank_connections = dmi_value(data->bank_connections);
+    info->bank_connections[0] = (bank_connections & 0x0Fu);
+    info->bank_connections[1] = (bank_connections & 0xF0u) >> 4;
+
+    // Decode speed and type
+    info->current_speed = dmi_value(data->current_speed);
+    info->current_type  = (dmi_memory_module_type_t){
+        ._value = dmi_value(data->current_type)
+    };
+
+    // Decode installed size
+    dmi_byte_t installed_size = dmi_value(data->installed_size) & 0x7Fu;
+    if (installed_size != 0x7Du)
+        info->has_installed_size = true;
+    if (installed_size != 0x7Fu)
+        info->installed_size = (dmi_size_t)(installed_size & 0x7Fu) << 20;
+
+    // Decode enabled size
+    dmi_byte_t enabled_size = dmi_value(data->enabled_size) & 0x7Fu;
+    if (enabled_size != 0x7Du)
+        info->has_enabled_size = true;
+    if (enabled_size != 0x7Fu) {
+        if (enabled_size == 0x7Eu)
+            info->is_disabled = true;
+        else if (enabled_size != 0x7Du)
+            info->enabled_size = (dmi_size_t)(enabled_size & 0x7Fu) << 20;
+    }
+
+    // Decode bank count
+    info->bank_count = dmi_value(data->enabled_size) & 0x80u ? 2 : 1;
+
+    // Decode error status
+    info->error_status = (dmi_memory_module_error_t){
+        ._value = dmi_value(data->error_status)
+    };
+
+    return info;
+}
+
+void dmi_memory_module_free(dmi_memory_module_t *info)
+{
+    free(info);
+}
