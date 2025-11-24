7f99c15e5b09b83729fb736864e85f839c0f1f58
Dmitry Sednev <dmitry@sednev.ru>
2025-11-24 22:45:33 +0300

    Implement firmware language information structure decoding #32

--- libopendmi/include/opendmi/table/firmware-language.h
+++ libopendmi/include/opendmi/table/firmware-language.h
@@ -11,15 +11,45 @@
 
 #include <opendmi/table.h>
 
+#ifndef DMI_FIRMWARE_LANGUAGE_T
+#define DMI_FIRMWARE_LANGUAGE_T
+typedef struct dmi_firmware_language dmi_firmware_language_t;
+#endif // !DMI_FIRMWARE_LANGUAGE_T
+
 #ifndef DMI_FIRMWARE_LANGUAGE_DATA_T
 #define DMI_FIRMWARE_LANGUAGE_DATA_T
 typedef struct dmi_firmware_language_data dmi_firmware_language_data_t;
 #endif // !DMI_FIRMWARE_LANGUAGE_DATA_T
 
-#ifndef DMI_FIRMWARE_LANGUAGE_T
-#define DMI_FIRMWARE_LANGUAGE_T
-typedef struct dmi_firmware_language dmi_firmware_language_t;
-#endif // !DMI_FIRMWARE_LANGUAGE_T
+#ifndef DMI_FIRMWARE_LANGUAGE_FLAGS_T
+#define DMI_FIRMWARE_LANGUAGE_FLAGS_T
+typedef union dmi_firmware_language_flags dmi_firmware_language_flags_t;
+#endif // !DMI_FIRMWARE_LANGUAGE_FLAGS_T
+
+/**
+ * @brief Firmware language flags
+ */
+DMI_PACKED_UNION(dmi_firmware_language_flags)
+{
+    /**
+     * @brief Raw value.
+     */
+    dmi_byte_t _value;
+
+    DMI_PACKED_STRUCT()
+    {
+        /**
+         * @brief If set to `true`, the current language strings use the
+         * abbreviated format. Otherwise, the strings use the long format.
+         */
+        bool is_abbreviated : 1;
+
+        /**
+         * @brief Reserved for future use.
+         */
+        dmi_byte_t reserved: 7;
+    };
+};
 
 /**
  * @brief Firmware language information table (type 13).
@@ -38,7 +68,7 @@ DMI_PACKED_STRUCT(dmi_firmware_language_data)
      * 
      * @since SMBIOS 2.0
      */
-    dmi_byte_t count;
+    dmi_byte_t language_count;
 
     /**
      * @brief Flags.
@@ -55,12 +85,43 @@ DMI_PACKED_STRUCT(dmi_firmware_language_data)
     /**
      * @brief String number (one-based) of the currently installed language.
      */
-    dmi_string_t curent_language;
+    dmi_string_t current_language;
+};
+
+struct dmi_firmware_language
+{
+    /**
+     * @brief Number of languages available.
+     */
+    size_t language_count;
+
+    /**
+     * @brief List of available languages.
+     */
+    const char **languages;
+
+    /**
+     * @brief If set to `true`, the language strings use the abbreviated
+     * format. Otherwise, the strings use the long format.
+     */
+    bool is_abbreviated;
+
+    /**
+     * @brief Currently installed language.
+     */
+    const char *current_language;
 };
 
 /**
  * @brief Firmware language information table specification.
  */
 extern const dmi_table_spec_t dmi_firmware_language_table;
+
+__BEGIN_DECLS
+
+dmi_firmware_language_t *dmi_firmware_language_decode(const dmi_table_t *table);
+void dmi_firmware_language_free(dmi_firmware_language_t *info);
+
+__END_DECLS
 
 #endif // !OPENDMI_TABLE_FIRMWARE_LANGUAGE_H
--- libopendmi/src/table/firmware-language.c
+++ libopendmi/src/table/firmware-language.c
@@ -4,10 +4,29 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 //
+#include <stdlib.h>
+
+#include <opendmi/utils.h>
 #include <opendmi/table/firmware-language.h>
 
 const dmi_attribute_t dmi_firmware_language_attrs[] =
 {
+    DMI_ATTRIBUTE(dmi_firmware_language_t, language_count, INTEGER, {
+        .code = "language-count",
+        .name = "Language count"
+    }),
+    DMI_ATTRIBUTE_ARRAY(dmi_firmware_language_t, languages, language_count, STRING, {
+        .code = "languages",
+        .name = "Languages"
+    }),
+    DMI_ATTRIBUTE(dmi_firmware_language_t, is_abbreviated, BOOL, {
+        .code = "is-abbreviated",
+        .name = "Abbreviated"
+    }),
+    DMI_ATTRIBUTE(dmi_firmware_language_t, current_language, STRING, {
+        .code = "current-language",
+        .name = "Current language"
+    }),
     DMI_ATTRIBUTE_NULL
 };
 
@@ -17,5 +36,46 @@ const dmi_table_spec_t dmi_firmware_language_table =
     .name       = "Firmware language information",
     .type       = DMI_TYPE_FIRMWARE_LANGUAGE,
     .min_length = 0x05,
-    .attributes = dmi_firmware_language_attrs
+    .attributes = dmi_firmware_language_attrs,
+    .handlers   = {
+        .decode = (dmi_table_decode_fn_t)dmi_firmware_language_decode,
+        .free   = (dmi_table_free_fn_t)dmi_firmware_language_free
+    }
 };
+
+dmi_firmware_language_t *dmi_firmware_language_decode(const dmi_table_t *table)
+{
+    dmi_firmware_language_t *info = nullptr;
+    dmi_firmware_language_data_t *data = dmi_cast(data, table->data);
+
+    info = calloc(1, sizeof(*info));
+    if (!info)
+        return nullptr;
+
+    info->language_count = dmi_value(data->language_count);
+
+    info->languages = calloc(info->language_count, sizeof(const char *));
+    if (!info->languages) {
+        free(info);
+        return nullptr;
+    }
+
+    for (size_t i = 0; i < info->language_count; i++) {
+        info->languages[i] = dmi_table_string(table, i + 1);
+    }
+
+    dmi_firmware_language_flags_t flags = {
+        ._value = dmi_value(data->flags)
+    };
+
+    info->is_abbreviated = flags.is_abbreviated;
+    info->current_language = dmi_table_string(table, data->current_language);
+
+    return info;
+}
+
+void dmi_firmware_language_free(dmi_firmware_language_t *info)
+{
+    free(info->languages);
+    free(info);
+}
