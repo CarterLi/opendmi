f346a1f259b4e6c1f3e11d9780a531fddea19dbb
Dmitry Sednev <dmitry@sednev.ru>
2025-11-28 11:24:13 +0300

    Implement management device threshold data structure decoding #35

--- libopendmi/include/opendmi/table/mgmt-device-threshold.h
+++ libopendmi/include/opendmi/table/mgmt-device-threshold.h
@@ -11,6 +11,11 @@
 
 #include <opendmi/table.h>
 
+#ifndef DMI_MGMT_DEVICE_THRESHOLD_DATA_T
+#define DMI_MGMT_DEVICE_THRESHOLD_DATA_T
+typedef struct dmi_mgmt_device_threshold_data dmi_mgmt_device_threshold_data_t;
+#endif // !DMI_MGMT_DEVICE_THRESHOLD_DATA_T
+
 /**
  * @brief Management device threshold data table (type 36).
  *
@@ -39,7 +44,7 @@ DMI_PACKED_STRUCT(dmi_mgmt_device_threshold_data)
     /**
      * @brief Upper non-critical threshold for this component.
      */
-    dmi_word_t upper_non_cricital;
+    dmi_word_t upper_non_critical;
 
     /**
      * @brief Lower critical threshold for this component.
@@ -62,9 +67,54 @@ DMI_PACKED_STRUCT(dmi_mgmt_device_threshold_data)
     dmi_word_t upper_non_recoverable;
 };
 
+#ifndef DMI_MGMT_DEVICE_THRESHOLD_T
+#define DMI_MGMT_DEVICE_THRESHOLD_T
+typedef struct dmi_mgmt_device_threshold dmi_mgmt_device_threshold_t;
+#endif // !DMI_MGMT_DEVICE_THRESHOLD_T
+
+struct dmi_mgmt_device_threshold
+{
+    /**
+     * @brief Lower non-critical threshold for this component.
+     */
+    short lower_non_critical;
+
+    /**
+     * @brief Upper non-critical threshold for this component.
+     */
+    short upper_non_critical;
+
+    /**
+     * @brief Lower critical threshold for this component.
+     */
+    short lower_critical;
+
+    /**
+     * @brief Upper critical threshold for this component.
+     */
+    short upper_critical;
+
+    /**
+     * @brief Lower non-recoverable threshold for this component.
+     */
+    short lower_non_recoverable;
+
+    /**
+     * @brief Upper non-recoverable threshold for this component.
+     */
+    short upper_non_recoverable;
+};
+
 /**
  * @brief Management device threshold data table specification.
  */
 extern const dmi_table_spec_t dmi_mgmt_device_threshold_table;
+
+__BEGIN_DECLS
+
+dmi_mgmt_device_threshold_t *dmi_mgmt_device_threshold_decode(const dmi_table_t *table);
+void dmi_mgmt_device_threshold_free(dmi_mgmt_device_threshold_t *info);
+
+__END_DECLS
 
 #endif // !OPENDMI_TABLE_MGMT_DEVICE_THRESHOLD_H
--- libopendmi/src/table/mgmt-device-threshold.c
+++ libopendmi/src/table/mgmt-device-threshold.c
@@ -4,10 +4,46 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 //
+#include <stdlib.h>
+#include <limits.h>
+
+#include <opendmi/context.h>
+#include <opendmi/utils.h>
+
 #include <opendmi/table/mgmt-device-threshold.h>
 
 const dmi_attribute_t dmi_mgmt_device_threshold_attrs[] =
 {
+    DMI_ATTRIBUTE(dmi_mgmt_device_threshold_t, lower_non_critical, INTEGER, {
+        .code    = "lower-non-critical",
+        .name    = "Lower non-critical",
+        .unknown = &(short){ SHRT_MIN }
+    }),
+    DMI_ATTRIBUTE(dmi_mgmt_device_threshold_t, upper_non_critical, INTEGER, {
+        .code    = "upper-non-critical",
+        .name    = "Upper non-critical",
+        .unknown = &(short){ SHRT_MIN }
+    }),
+    DMI_ATTRIBUTE(dmi_mgmt_device_threshold_t, lower_critical, INTEGER, {
+        .code    = "lower-critical",
+        .name    = "Lower critical",
+        .unknown = &(short){ SHRT_MIN }
+    }),
+    DMI_ATTRIBUTE(dmi_mgmt_device_threshold_t, upper_critical, INTEGER, {
+        .code    = "upper-critical",
+        .name    = "Upper critical",
+        .unknown = &(short){ SHRT_MIN }
+    }),
+    DMI_ATTRIBUTE(dmi_mgmt_device_threshold_t, lower_non_recoverable, INTEGER, {
+        .code    = "lower-non-recoverable",
+        .name    = "Lower non-recoverable",
+        .unknown = &(short){ SHRT_MIN }
+    }),
+    DMI_ATTRIBUTE(dmi_mgmt_device_threshold_t, upper_non_recoverable, INTEGER, {
+        .code    = "upper-non-recoverable",
+        .name    = "Upper non-recoverable",
+        .unknown = &(short){ SHRT_MIN }
+    }),
     DMI_ATTRIBUTE_NULL
 };
 
@@ -18,5 +54,46 @@ const dmi_table_spec_t dmi_mgmt_device_threshold_table =
     .type        = DMI_TYPE_MGMT_DEVICE_THRESHOLD,
     .min_version = DMI_VERSION(2, 3, 0),
     .min_length  = 0x10,
-    .attributes  = dmi_mgmt_device_threshold_attrs
+    .attributes  = dmi_mgmt_device_threshold_attrs,
+    .handlers    = {
+        .decode = (dmi_table_decode_fn_t)dmi_mgmt_device_threshold_decode,
+        .free   = (dmi_table_free_fn_t)dmi_mgmt_device_threshold_free
+    }
 };
+
+dmi_mgmt_device_threshold_t *dmi_mgmt_device_threshold_decode(const dmi_table_t *table)
+{
+    dmi_mgmt_device_threshold_t *info;
+    dmi_mgmt_device_threshold_data_t *data;
+
+    if (!table) {
+        dmi_set_error(nullptr, DMI_ERROR_INVALID_ARGUMENT);
+        return nullptr;
+    }
+    if (table->type != DMI_TYPE_MGMT_DEVICE_THRESHOLD) {
+        dmi_set_error(table->context, DMI_ERROR_INVALID_TABLE_TYPE);
+        return nullptr;
+    }
+
+    info = calloc(1, sizeof(*info));
+    if (!info) {
+        dmi_set_error(table->context, DMI_ERROR_OUT_OF_MEMORY);
+        return nullptr;
+    }
+
+    data = dmi_cast(data, table->data);
+
+    info->lower_non_critical    = dmi_value(data->lower_non_critical);
+    info->upper_non_critical    = dmi_value(data->upper_non_critical);
+    info->lower_critical        = dmi_value(data->lower_critical);
+    info->upper_critical        = dmi_value(data->upper_critical);
+    info->lower_non_recoverable = dmi_value(data->lower_non_recoverable);
+    info->upper_non_recoverable = dmi_value(data->upper_non_recoverable);
+
+    return info;
+}
+
+void dmi_mgmt_device_threshold_free(dmi_mgmt_device_threshold_t *info)
+{
+    free(info);
+}
