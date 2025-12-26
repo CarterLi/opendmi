d96e09bb08bec0bd0ce1334c2c90acf9f2c9576f
Dmitry Sednev <dmitry@sednev.ru>
2025-12-27 01:17:32 +0300

    Add skeleton for YAML format handler

--- opendmi/src/format/yaml.c
+++ opendmi/src/format/yaml.c
@@ -6,8 +6,27 @@
 //
 #include <opendmi/format/yaml.h>
 
+static void *dmi_yaml_initialize(dmi_context_t *context, FILE *stream);
+
+static void dmi_yaml_finalize(void *asession);
+
 const dmi_format_t dmi_yaml_format =
 {
-    .code = "yaml",
-    .name = "YAML"
+    .code     = "yaml",
+    .name     = "YAML",
+    .handlers = {
+        .initialize = dmi_yaml_initialize,
+        .finalize   = dmi_yaml_finalize
+    }
 };
+
+static void *dmi_yaml_initialize(dmi_context_t *context, FILE *stream)
+{
+    DMI_UNUSED(context);
+    DMI_UNUSED(stream);
+}
+
+static void dmi_yaml_finalize(void *asession)
+{
+    DMI_UNUSED(asession);
+}
