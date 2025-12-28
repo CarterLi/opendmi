74bb953a5c8728005a76ea5e3f6c3adf6c56e2ba
Dmitry Sednev <dmitry@sednev.ru>
2025-12-29 01:16:39 +0300

    Introduce attribute formatting tests

---
+++ libopendmi/test/attribute-format-size.c
@@ -0,0 +1,15 @@
+//
+// OpenDMI: Cross-platform DMI/SMBIOS framework
+// Copyright (c) 2025, Dmitry Sednev <dmitry@sednev.ru>
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#include <sysexits.h>
+
+#include <opendmi/attribute.h>
+
+int main(void)
+{
+    // TODO: Test dmi_attribute_format_size()
+    return EX_UNAVAILABLE;
+}
