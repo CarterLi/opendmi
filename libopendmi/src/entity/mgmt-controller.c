c2fa7086efef505e617c7199b8d7f5922ebc1e20
xerrial <dimass1129@gmail.com>
2026-01-02 20:48:06 +1000

    Added mgmt-controller name sets

--- libopendmi/src/entity/mgmt-controller.c
+++ libopendmi/src/entity/mgmt-controller.c
@@ -8,18 +8,143 @@
 #include <opendmi/utils.h>
 #include <memory.h>
 
+const dmi_name_set_t dmi_mgmt_if_type_names =
+{
+    .code = "mgmt-if-types",
+    .names = {
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_KCS,
+            .code = "keyboard-controller-style",
+            .name = "Keyboard Controller Style"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_8250_UART,
+            .code = "8250-uart",
+            .name = "8250 UART Register Compatible"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_16450_UART,
+            .code = "16450-uart",
+            .name = "16450 UART Register Compatible"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_16550_UART,
+            .code = "16550-uart",
+            .name = "16550/16550A UART Register Compatible"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_16650_UART,
+            .code = "16650-uart",
+            .name = "16650/16650A UART Register Compatible"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_16750_UART,
+            .code = "16750-uart",
+            .name = "16750/16750A UART Register Compatible"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_16850_UART,
+            .code = "16850-uart",
+            .name = "16850/16850A UART Register Compatible"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_I2C_SMBUS,
+            .code = "i2c-smbus",
+            .name = "I2C/SMBUS"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_I3C,
+            .code = "i3c",
+            .name = "I3C"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_PCIE_VDM,
+            .code = "pcie-vdm",
+            .name = "PCIe VDM"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_MMBI,
+            .code = "mmbi",
+            .name = "MMBI"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_PCC,
+            .code = "pcc",
+            .name = "PCC"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_UCIE,
+            .code = "ucie",
+            .name = "UCIe"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_MCTP_USB,
+            .code = "usb",
+            .name = "USB"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_NETWORK_HOST_IF,
+            .code = "network-host-interface",
+            .name = "Network Host Interface"
+        },
+        {
+            .id   = DMI_MGMT_IF_TYPE_OEM,
+            .code = "oem-defined",
+            .name = "OEM-defined"
+        },
+        DMI_NAME_NULL
+    }
+};
+
+const dmi_name_set_t dmi_mgmt_protocol_names =
+{
+    .code = "mgmt-protocols",
+    .names = {
+        {
+            .id   = DMI_MGMT_PROTOCOL_IPMI,
+            .code = "ipmi",
+            .name = "IPMI"
+        },
+        {
+            .id   = DMI_MGMT_PROTOCOL_MCTP,
+            .code = "mctp",
+            .name = "MCTP"
+        },
+        {
+            .id   = DMI_MGMT_PROTOCOL_REDFISH_OVER_IP,
+            .code = "redfish-over-ip",
+            .name = "Redfish over IP"
+        },
+        {
+            .id   = DMI_MGMT_PROTOCOL_OEM,
+            .code = "oem-defined",
+            .name = "OEM-defined"
+        },
+        DMI_NAME_NULL
+    }
+};
+
 const dmi_attribute_t dmi_mgmt_controller_host_if_attrs[] =
 {
+    DMI_ATTRIBUTE(dmi_mgmt_controller_t, if_type, ENUM, {
+        .code    = "if-type",
+        .name    = "Interface Type",
+        .values  = &dmi_mgmt_if_type_names
+    }),
     DMI_ATTRIBUTE_NULL
 };
 
 const dmi_entity_spec_t dmi_mgmt_controller_host_if_spec =
 {
-    .code        = "mgmt-controller-host-if",
-    .name        = "Management controller host interface",
-    .type        = DMI_TYPE_MGMT_CONTROLLER_HOST_IF,
-    .min_length  = 0x06,
-    .attributes  = dmi_mgmt_controller_host_if_attrs
+    .code       = "mgmt-controller-host-if",
+    .name       = "Management controller host interface",
+    .type       = DMI_TYPE_MGMT_CONTROLLER_HOST_IF,
+    .min_length = 0x06,
+    .attributes = dmi_mgmt_controller_host_if_attrs,
+    .handlers   = {
+        .decode = (dmi_entity_decode_fn_t)dmi_mgmt_controller_decode,
+        .free   = (dmi_entity_free_fn_t)dmi_mgmt_controller_free
+    }
 };
 
 dmi_mgmt_controller_t *
