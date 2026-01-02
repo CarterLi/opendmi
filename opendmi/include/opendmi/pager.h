ff5c9093093908b3733af4d3b560c087511001c2
Dmitry Sednev <dmitry@sednev.ru>
2026-01-02 20:58:19 +0300

    Add support for $PAGER #64

---
+++ opendmi/include/opendmi/pager.h
@@ -0,0 +1,20 @@
+//
+// OpenDMI: Cross-platform DMI/SMBIOS framework
+// Copyright (c) 2025-2026, The OpenDMI contributors
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#ifndef OPENDMI_PAGER_H
+#define OPENDMI_PAGER_H
+
+#pragma once
+
+#include <opendmi/types.h>
+
+__BEGIN_DECLS
+
+bool dmi_pager_start(dmi_context_t *context);
+
+__END_DECLS
+
+#endif // !OPENDMI_PAGER_H
