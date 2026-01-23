dcdf3a4b90a9a0b31db8affe3b382e3d655c5f14
Dmitry Sednev <dmitry@sednev.ru>
2026-01-23 04:25:26 +0300

    Introduce regression script

---
+++ regress.py
@@ -0,0 +1,58 @@
+#
+# OpenDMI: Cross-platform DMI/SMBIOS framework
+# Copyright (c) 2025-2026, The OpenDMI contributors
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+import os
+import subprocess
+import yaml
+from yaml.loader import SafeLoader
+
+base_dir = os.path.dirname(os.path.realpath(__file__))
+opendmi_dir = os.path.join(base_dir, "build/opendmi")
+data_dir = os.path.join(base_dir, "data")
+
+def data_files():
+    for root, _, files in os.walk(data_dir):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+
+            file_base, file_ext = os.path.splitext(file_path)
+            if file_ext != ".bin":
+                continue
+
+            yield file_path, file_base + ".yaml"
+
+def load_spec(spec_path: str):
+    with open(spec_path) as spec_file:
+        return yaml.load(spec_file, Loader=SafeLoader)
+
+def load_dump(dump_path: str):
+    try:
+        process = subprocess.Popen(
+            [f"{opendmi_dir}/bin/opendmi", "--from-dump", dump_path, "--format=yaml"],
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE
+        )
+        stdout, stderr = process.communicate()
+
+        return yaml.load(stdout, Loader=SafeLoader)
+    except subprocess.CalledProcessError as e:
+        print("FAILED:")
+        print(e.stderr)
+
+def process_file(dump_path: str, spec_path: str):
+    spec = load_spec(spec_path)
+    data = load_dump(dump_path)
+
+def main():
+    for dump_path, spec_path in data_files():
+        if os.path.isfile(spec_path):
+            print(f"Processing {dump_path}")
+            process_file(dump_path, spec_path)
+        else:
+            print(f"Skipping {dump_path}")
+
+if __name__ == "__main__":
+	main()
