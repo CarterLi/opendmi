197ad6d2837db94e8ed94650845fad8fe4a9d24b
Dmitry Sednev <dmitry@sednev.ru>
2026-02-04 19:57:41 +0300

    Migrate slot tests to CMocka

---
+++ libopendmi/test/entity/slot.c
@@ -0,0 +1,46 @@
+//
+// OpenDMI: Cross-platform DMI/SMBIOS framework
+// Copyright (c) 2025-2026, The OpenDMI contributors
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#include <stdlib.h>
+#include <stdbool.h>
+#include <cmocka.h>
+
+#include <opendmi/entity/slot.h>
+
+static void test_slot_usage_name(void **state);
+static void test_slot_width_name(void **state);
+
+int main(void)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_slot_usage_name),
+        cmocka_unit_test(test_slot_width_name)
+    };
+
+    return cmocka_run_group_tests(tests, nullptr, nullptr);
+}
+
+static void test_slot_usage_name(void **state)
+{
+    dmi_unused(state);
+
+    assert_null(dmi_slot_usage_name(__DMI_SLOT_USAGE_COUNT));
+
+    for (int i = 0; i < __DMI_SLOT_USAGE_COUNT; i++) {
+        assert_non_null(dmi_slot_usage_name(i));
+    }
+}
+
+static void test_slot_width_name(void **state)
+{
+    dmi_unused(state);
+
+    assert_null(dmi_slot_width_name(__DMI_SLOT_WIDTH_COUNT));
+
+    for (int i = 0; i < __DMI_SLOT_WIDTH_COUNT; i++) {
+        assert_non_null(dmi_slot_width_name(i));
+    }
+}
