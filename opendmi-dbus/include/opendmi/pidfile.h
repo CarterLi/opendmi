aba46f71cf05c9ecad13d1175a4342e5821ed47e
Dmitry Sednev <dmitry@sednev.ru>
2026-04-08 10:16:18 +0300

    Add D-bus service skeleton

---
+++ opendmi-dbus/include/opendmi/pidfile.h
@@ -0,0 +1,30 @@
+//
+// OpenDMI: Cross-platform DMI/SMBIOS framework
+// Copyright (c) 2025-2026, The OpenDMI contributors
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#ifndef __OPENDMI_PIDFILE_H__
+#define __OPENDMI_PIDFILE_H__
+
+#pragma once
+
+#include <opendmi/types.h>
+
+typedef struct dmi_pidfile *dmi_pidfile_t;
+
+struct dmi_pidfile
+{
+    char *path;
+    int fd;
+};
+
+__BEGIN_DECLS
+
+pid_t dmi_pidfile_read(const char *path);
+dmi_pidfile_t *dmi_pidfile_acquire(const char *path);
+void dmi_pidfile_release(dmi_pidfile_t *pidfile);
+
+__END_DECLS
+
+#endif // !__OPENDMI_PIDFILE_H__
---
+++ opendmi-dbus/src/main.c
@@ -0,0 +1,16 @@
+//
+// OpenDMI: Cross-platform DMI/SMBIOS framework
+// Copyright (c) 2025-2026, The OpenDMI contributors
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#include <stdlib.h>
+#include <opendmi/types.h>
+
+int main(int argc, const char *argv[])
+{
+    dmi_unused(argc);
+    dmi_unused(argv);
+
+    return EXIT_SUCCESS;
+}
